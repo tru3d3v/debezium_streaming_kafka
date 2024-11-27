f8bdd861b0ec1f1c746d09a6cb2da70610c52e05
Muhamad Muchlis <muhamad.muchlis@ifg-life.id>
2024-11-27 22:59:59 +0700

    add new

---
+++ datatest/newdata.sql
@@ -0,0 +1,5 @@
+INSERT INTO db1.data_pelanggan (id, ktp, nama_lengkap, pob, dob, gender) VALUES
+(1, '3201234567890001', 'Budi Santoso', 'Jakarta', '1990-08-17', 'L'),
+(2, '3201234567890002', 'Ani Rahmawati', 'Bandung', '1995-03-12', 'P'),
+(3, '3201234567890003', 'Citra Dewi', 'Surabaya', '1988-11-25', 'P'),
+(4, '3201234567890004', 'Dodi Setiawan', 'Medan', '1992-06-05', 'L');
